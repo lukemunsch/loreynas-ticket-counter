dbb2ca45b258dbd0ae27eb31e4e8c236c608f7ed
lukemunsch <lpmunsch@gmail.com>
2022-04-21 21:45:58 +0000

    add templating and divisibleby responsivity to destinations page

--- destinations/models.py
+++ destinations/models.py
@@ -76,12 +76,6 @@ class Destination(models.Model):
     dest_description = models.TextField()
     hotspot = models.BooleanField(default=False, null=True, blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    rating = models.DecimalField(
-        max_digits=5,
-        decimal_places=1,
-        null=True,
-        blank=True
-    )
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
