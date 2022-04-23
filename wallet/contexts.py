a282157baee1faf4d92548da884c28a573577a5a
lukemunsch <lpmunsch@gmail.com>
2022-04-23 18:56:19 +0000

    add context content for ticket wallet

--- wallet/contexts.py
+++ wallet/contexts.py
@@ -6,14 +6,25 @@ def wallet_contents(request):
     wallet_tickets = []
     total = 0
     ticket_count = 0
+    wallet = request.session.get('wallet', {})
 
     if ticket_count >= settings.DISCOUNT_WALLET_THRESHOLD:
         discount = total * Decimal(settings.STANDARD_DISCOUNT_PERCENTAGE / 100)
         discount_wallet_delta = settings.DISCOUNT_WALLET_THRESHOLD - ticket_count
     else:
         discount = 0
+        discount_wallet_delta = 0
 
-    grand_total = total
+    grand_total = total - discount
 
-    context = {}
+    context = {
+        'wallet_tickets': wallet_tickets,
+        'total': total,
+        'ticket_count': ticket_count,
+        'discount': discount,
+        'discount_wallet_delta': discount_wallet_delta,
+        'discount_wallet_threshold': settings.DISCOUNT_WALLET_THRESHOLD,
+        'standard_discount_percentage': settings.STANDARD_DISCOUNT_PERCENTAGE,
+        'grand_total': grand_total,
+    }
     return context
