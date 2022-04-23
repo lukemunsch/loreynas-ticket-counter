e9497a1d3fc540b856b31bd5398a737672326b90
lukemunsch <lpmunsch@gmail.com>
2022-04-23 22:12:00 +0000

    add new view for adding new tickets to wallet

--- wallet/urls.py
+++ wallet/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.view_wallet, name='view_wallet')
+    path('', views.view_wallet, name='view_wallet'),
+    path('add/<ticket_id>/', views.add_to_wallet, name='add_to_wallet'),
 ]
--- wallet/views.py
+++ wallet/views.py
@@ -1,7 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from destinations.models import Destination
 
 
 def view_wallet(request):
     """set up our wallet view to hold our 
     tickets for purchasing when ready"""
     return render(request, 'wallet/wallet.html')
+
+
+def add_to_wallet(request, ticket_id):
+    """Add a quantity of a specific ticket to the wallet"""
+
+    destination = get_object_or_404(Destination, pk=ticket_id)
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    wallet = request.session.get('wallet', {})
+
+    if ticket_id in list(wallet.keys()):
+        wallet[ticket_id] += quantity
+    else:
+        wallet[ticket_id] = quantity
+    
+    request.session['wallet'] = wallet
+    return redirect(redirect_url)
