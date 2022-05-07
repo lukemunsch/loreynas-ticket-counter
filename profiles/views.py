ffdddeb77a3074543c7b4b19befa18760b0561c3
lukemunsch <lpmunsch@gmail.com>
2022-05-07 22:13:26 +0000

    Add code for order history view

--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
 
+from checkout.models import Order
 from .models import UserProfile
 from .forms import UserProfileForm
 
@@ -27,6 +28,23 @@ def profile(request):
         'form': form,
         'orders': orders,
         'on_profile_page': True,
+    }
+
+    return render(request, template, context)
+
+
+def order_history(request, order_number):
+    """set up our order history for user profile template"""
+    order = get_object_or_404(Order, order_number=order_number)
+    messages.info(request, (
+        f'This is a past confirmation for the order number {order_number}. '
+        f'A confirmation email was sent out on the order date'
+    ))
+
+    template = 'checout/checkout_success.html'
+    context = {
+        'order': order,
+        'from_profile': True,
     }
 
     return render(request, template, context)
