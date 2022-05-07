4d77fd866adf655352d43597dd87c05effd852d9
lukemunsch <lpmunsch@gmail.com>
2022-05-07 21:30:22 +0000

    add form function to our profile page

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
 
 from .models import UserProfile
 from .forms import UserProfileForm
@@ -7,13 +8,25 @@ from .forms import UserProfileForm
 def profile(request):
     """display the user's profle"""
     profile = get_object_or_404(UserProfile, user=request.user)
-    form = UserProfileForm(instance=profile)
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile Successfully Updated')
+        else:
+            messages.error(request, (
+                'Update failed. Please ensure '
+                'the form is valid.'))
+    else:
+        form = UserProfileForm(instance=profile)
     orders = profile.orders.all()
 
     template = 'profiles/profile.html'
     context = {
+        'profile': profile,
         'form': form,
         'orders': orders,
+        'on_profile_page': True,
     }
 
     return render(request, template, context)
