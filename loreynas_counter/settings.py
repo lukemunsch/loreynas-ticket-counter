adafc43ad442c2e09d4c8b1e6621d6116b160b0d
lukemunsch <lpmunsch@gmail.com>
2022-05-12 19:53:07 +0000

    Fix attempt for heroku deployment

--- loreynas_counter/settings.py
+++ loreynas_counter/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://loreynas-ticket-counter.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['https://loreynas-ticket-counter.herokuapp.com/', 'localhost']
 
 
 # Application definition
