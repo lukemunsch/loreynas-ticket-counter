370e45cb96ab3bd870edc4f264518b204bd6e0e7
Luke Munsch <lpmunsch@gmail.com>
2026-03-20 11:44:01 +0000

    updateware component again

--- loreynas_counter/settings.py
+++ loreynas_counter/settings.py
@@ -54,7 +54,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.contrib.sessions.middleware.AccountMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
