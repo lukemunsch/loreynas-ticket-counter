eb23396e38fd2b697179b64ab4c7ce4f0dc599c4
Luke Munsch <lpmunsch@gmail.com>
2026-03-27 21:24:54 +0000

    fix the settings spelling mistake for allauth verifications

--- loreynas_counter/settings.py
+++ loreynas_counter/settings.py
@@ -210,7 +210,7 @@ STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
-if 'DEVELOPEMENT' in os.environ:
+if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
     DEFAULT_FROM_EMAIL = 'loreynascounter@aetheryte.com'
 else:
